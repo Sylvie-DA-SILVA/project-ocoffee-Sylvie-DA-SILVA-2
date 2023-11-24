650845b351b5e8fc2d942d5454fbb6e7b1b0c902
enzoclock <enzo.testa@oclock.io>
2023-11-24 14:57:11 +0100

    config: ajout de ESLint

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+// Import NPM module
 const express = require("express"); // Syntaxe CJS
 
+// Create Express app
 const app = express();
 
+// Configure Express app
 app.get("/", (req, res) => {
   res.send("Hello !");
 });
 
+// Start server
 const port = 3000;
 app.listen(port, () => {
-  console.log(`🚀 oCoffee app started at http://localhost:${port}`)
-})
+  console.log(`🚀 oCoffee app started at http://localhost:${port}`);
+});

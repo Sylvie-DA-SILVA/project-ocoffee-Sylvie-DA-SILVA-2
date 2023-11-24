3f5da8ebe4b020a4ef036e1f25b459fff997f5ca
enzoclock <enzo.testa@oclock.io>
2023-11-24 14:59:34 +0100

    feat: configure view engine

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ const express = require("express"); // Syntaxe CJS
 // Create Express app
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
 // Configure Express app
 app.get("/", (req, res) => {
-  res.send("Hello !");
+  res.render("pages/home");
+});
+
+app.get("/catalog", (req, res) => {
+  res.render("pages/catalog");
 });
 
 // Start server

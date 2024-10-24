18998e8a51ab818b226370b270361e8a2f4d2d15
SylvieDaSilva <s.dasilvaferraz@gmail.com>
2024-10-24 16:32:47 +0200

    maj du code global + ajout de fonctionnalites, page détail + affichage dynamique + maj controller

--- controllers/main-controller.js
+++ controllers/main-controller.js
@@ -6,12 +6,15 @@ const mainController = {
     res.render("pages/home", { articles });
   },
 
-  renderCatalogPage(req, res) {
-    res.render("pages/catalog");
+  async renderCatalogPage(req, res) {
+    const articles = await dataMapper.getAllProducts();
+    res.render("pages/catalog", { articles });
   },
 
-  renderCoffeeDetailsPage(req, res) {
-    res.render("pages/article");
+  async renderCoffeeDetailsPage(req, res) {
+    const productId = req.params.id; // Récupérer l'ID du produit depuis l'URL
+    const product = await dataMapper.getProductById(productId);
+    res.render("pages/article", { product });
   }
 };
 
--- database/dataMapper.js
+++ database/dataMapper.js
@@ -5,6 +5,30 @@ const dataMapper = {
     const result = await db.query(`SELECT * FROM coffee ORDER BY created_at DESC LIMIT 3`);
     const products = result.rows;
     return products;
+  },
+
+  async getAllProducts() {
+    const result = await db.query('SELECT * FROM coffee ORDER BY created_at DESC');
+    const products = result.rows;
+    return products;
+  },
+
+  // async getProductById(productId) {
+  //   const query = 'SELECT * FROM coffee WHERE id = $1';
+  //   const result = await db.query(query, [productId]);
+  //   const product = result.rows[0]; // On suppose que l'ID est unique, donc un seul produit sera retourné
+  //   return product;
+  // },
+
+  async getProductById(productId) {
+    const query = `
+    SELECT coffee.*, category.name AS category_name
+    FROM coffee
+    JOIN category ON coffee.category_id = category.id
+    WHERE coffee.id = $1 `;
+    const result = await db.query(query, [productId]);
+    const product = result.rows[0]; // On suppose que l'ID est unique, donc un seul produit sera retourné
+    return product;
   }
 };
 
